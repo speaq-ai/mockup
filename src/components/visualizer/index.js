4a38f302b0368db5c8a1250833209787a08a424b
ianHansborough <ian.hansborough@gmail.com>
2019-09-05 14:55:48 -0400

    added voice wave gif

--- src/components/visualizer/index.js
+++ src/components/visualizer/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import KeplerGl from "kepler.gl";
-import { addDataToMap } from 'kepler.gl/actions';
-import { processCsvData } from 'kepler.gl/processors';
+import { addDataToMap } from "kepler.gl/actions";
+import { processCsvData } from "kepler.gl/processors";
 import store from "@/ducks";
-import mockupData from '../../data/mockupData';
+import mockupData from "../../data/mockupData";
 
 class Visualizer extends Component {
 	constructor(props) {
@@ -21,8 +21,8 @@ class Visualizer extends Component {
 		return (
 			<div>
 				<div style={buttonDivStyle}>
-                	<RecordButton width="25px" height="25px" onClick={this.loadMap}/>
-            	</div>
+					<RecordButton width="25px" height="25px" onClick={this.loadMap} />
+				</div>
 				<KeplerGl
 					id="foo"
 					store={store}
@@ -41,49 +41,61 @@ class Visualizer extends Component {
 				datasets: [
 					{
 						info: {
-							label: 'Sacramento Real Estate Transactions',
-							id: 'mockup_data'
+							label: "Sacramento Real Estate Transactions",
+							id: "mockup_data",
 						},
-						data: processCsvData(mockupData)
-					}
-				]
+						data: processCsvData(mockupData),
+					},
+				],
 			})
 		);
 	}
 }
 
 class RecordButton extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            display: "images/icons/microphone.png"
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
- 
-    handleClick() {
-        setTimeout(this.props.onClick, 3000)      
-    }
- 
-    render() {
-        return (
-            <button onClick={this.handleClick} style={buttonStyle}>
-                <img src={this.state.display} width={this.props.width} height={this.props.height}/>
-            </button>
-        )
-    } 
+	constructor(props) {
+		super(props);
+		this.state = {
+			display: "images/icons/microphone.png",
+		};
+		this.handleClick = this.handleClick.bind(this);
+	}
+
+	handleClick() {
+		const { display } = this.state;
+		this.setState({
+			display: "images/icons/speech.gif",
+		});
+		setTimeout(() => {
+			this.setState({ display: "images/icons/microphone.png" });
+			this.props.onClick();
+		}, 3000);
+	}
+
+	render() {
+		return (
+			<button onClick={this.handleClick} style={buttonStyle}>
+				<img
+					src={this.state.display}
+					width={this.props.width}
+					height={this.props.height}
+				/>
+			</button>
+		);
+	}
 }
- 
+
 const buttonDivStyle = {
-    backgroundColor: '#2f404a',
-    textAlign: 'center'
+	backgroundColor: "#2f404a",
+	textAlign: "center",
 };
- 
+
 const buttonStyle = {
-    backgroundColor: '#2c2d2d',
-    borderColor: '#2c2d2d',
-    width: '50px'
-}
+	backgroundColor: "#2c2d2d",
+	borderColor: "#2c2d2d",
+	width: "50px",
+	outline: "None",
+};
 
 const mapStateToProps = state => state;
 
