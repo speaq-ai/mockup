d0030a8d7907211b94c2725a853fb500e443d256
Joe Forsman <jrforsman@gmail.com>
2019-09-05 09:18:04 -0400

    button connectivity working, formatting data now

--- src/components/visualizer/index.js
+++ src/components/visualizer/index.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import KeplerGl from "kepler.gl";
+import { addDataToMap, processCsvData } from 'kepler.gl/actions';
 import store from "@/ducks";
+import mockupData from '../../data/mockupData';
 
 export default class Visualizer extends Component {
+	constructor(props) {
+		super(props);
+
+		// resolve missing this
+		this.loadMap = this.loadMap.bind(this);
+	}
+
 	render() {
 		const width = window.innerWidth;
 		const height = window.innerHeight;
 		const mapboxAccessToken = process.env.MAPBOX_ACCESS_TOKEN;
 		return (
 			<div>
+				<button type="button" value="Talk" onClick={this.loadMap} />
 				<KeplerGl
 					id="foo"
 					store={store}
@@ -17,6 +27,23 @@ export default class Visualizer extends Component {
 					height={height}
 				/>
 			</div>
+		);
+	}
+
+	loadMap() {
+		// load sample data and process csv file
+		this.props.dispatch(
+			addDataToMap({
+				datasets: [
+					{
+						info: {
+							label: 'Sacramento Real Estate Transactions',
+							id: 'mockup_data'
+						},
+						data: processCsvData(mockupData)
+					}
+				]
+			})
 		);
 	}
 }
